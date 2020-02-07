d990e6916576343c529174b6af0828b54e00a169
KPlackowski <kpchris20@gmail.com>
2020-02-08 00:47:05 +0100

    rebuild database futher

--- orders/admin.py
+++ orders/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import Topping, ItemKind, ItemName, ItemSize, Menu
+from .models import Topping, ItemKind, ItemName, ItemSize, MenuItem
 # Register your models here.
 
 admin.site.register(Topping)
 admin.site.register(ItemKind)
 admin.site.register(ItemName)
 admin.site.register(ItemSize)
-admin.site.register(Menu)
+admin.site.register(MenuItem)
--- orders/views.py
+++ orders/views.py
@@ -2,11 +2,18 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.template.defaulttags import register
+from django.core.exceptions import ObjectDoesNotExist
 
-from collections import defaultdict, namedtuple
-from ordered_set import OrderedSet
+from .models import Topping, ItemKind, ItemName, ItemSize, MenuItem
 
-from .models import Topping, ItemKind, ItemName, ItemSize, Menu
+@register.filter
+def get_size(instance, key):
+    try:
+        result = instance.get(size_parent=key)
+    except ObjectDoesNotExist:
+        result = ""
+    return result
 
 # Create your views here.
 def index(request):
@@ -17,6 +24,6 @@ def index(request):
     return render(request, "orders/index.html", context)
 
 def item_info(request, menu_id):
-    item = Menu.objects.get(id=menu_id)
+    item = MenuItem.objects.get(id=menu_id)
     context = {"item": item}
     return render(request, "orders/item_info.html", context)
